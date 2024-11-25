129cf79d12a69ad6bb8cccf3b4047113b5d94fb0
Faisal Alrico <if23.faisalalrico@mhs.ubpkarawang.ac.id>
2024-11-26 05:46:24 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("hamburger").onclick = function() {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("show"); // Mengubah kelas untuk menampilkan/menghilangkan tautan
+};
